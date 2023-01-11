37da73969773fe09722a8e9e2b726033189a45a2
Yassine Zeriouh <y@zeriouh.io>
2023-01-11 18:12:33 +0100

    feat(bookings): add booking-item component

---
+++ apps/frontend/src/app/bookings/components/booking-item/booking-item.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'booking-app-booking-item',
+  templateUrl: './booking-item.component.html',
+  styleUrls: ['./booking-item.component.css'],
+})
+export class BookingItemComponent {}
---
+++ apps/frontend/src/app/bookings/components/components.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BookingItemComponent } from './booking-item/booking-item.component';
+
+@NgModule({
+  declarations: [BookingItemComponent],
+  imports: [CommonModule],
+})
+export class ComponentsModule {}
