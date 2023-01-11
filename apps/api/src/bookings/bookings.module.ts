23232be1f683f547feae325201d5169ed80f655f
Yassine Zeriouh <y@zeriouh.io>
2023-01-11 22:12:12 +0100

    feat(bookings): implement api bookings service

--- apps/api/src/bookings/bookings.module.ts
+++ apps/api/src/bookings/bookings.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BookingsService } from './bookings.service';
 import { BookingsController } from './bookings.controller';
+import {PrismaService} from "../shared/prisma.service";
 
 @Module({
-  providers: [BookingsService],
+  providers: [PrismaService, BookingsService],
   controllers: [BookingsController],
 })
 export class BookingsModule {}
--- apps/api/src/bookings/bookings.service.ts
+++ apps/api/src/bookings/bookings.service.ts
@@ -1,5 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import {PrismaService} from "../shared/prisma.service";
 
 @Injectable()
 export class BookingsService {
+
+  constructor(private prisma: PrismaService) {
+  }
+
+  async getAll() {
+    return this.prisma.booking.findMany();
+  }
+
+  async create(data) {
+    return this.prisma.booking.create({
+      data
+    });
+  }
 }
