3d8673016f7efe627a98d50fd69f00f9c099ce9b
Yassine Zeriouh <y@zeriouh.io>
2023-01-12 16:12:07 +0100

    feat(api): add global validation pipe

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,13 +3,16 @@
  * This is only a minimal backend to get started.
  */
 
-import {Logger} from '@nestjs/common';
+import {Logger, ValidationPipe} from '@nestjs/common';
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3333;
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+  }));
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}`
