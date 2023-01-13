5aed1ebc1c41dd22c8c9cd2587c5ff742a2f6908
Yassine Zeriouh <y@zeriouh.io>
2023-01-13 01:20:18 +0100

    style(users): remove unused imports in user create dto

--- libs/dto/src/lib/users/create.dto.ts
+++ libs/dto/src/lib/users/create.dto.ts
@@ -1,5 +1,5 @@
 import {Prisma} from "@prisma/client";
-import {IsEmail, IsNotEmpty, IsString, Matches, MaxLength, MinLength} from "class-validator";
+import {IsEmail, IsNotEmpty, IsString, Matches} from "class-validator";
 
 export class CreateUserDto implements Prisma.UserCreateInput {
   @IsNotEmpty()
