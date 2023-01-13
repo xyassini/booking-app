5825e4e0f7b3c26dc853a37eaf9aa8d6f7185c29
Yassine Zeriouh <y@zeriouh.io>
2023-01-13 14:40:20 +0100

    test(bookings-create): create component test

--- apps/frontend/src/app/bookings/bookings-create/bookings-create.component.spec.ts
+++ apps/frontend/src/app/bookings/bookings-create/bookings-create.component.spec.ts
@@ -1,16 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { BookingsCreateComponent } from './bookings-create.component';
+import "reflect-metadata"
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BookingsCreateComponent} from './bookings-create.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {ToastrModule} from "ngx-toastr";
 
 describe('BookingsCreateComponent', () => {
   let component: BookingsCreateComponent;
   let fixture: ComponentFixture<BookingsCreateComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [BookingsCreateComponent],
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [FormsModule, ReactiveFormsModule, HttpClientTestingModule, ToastrModule.forRoot()],
+      declarations: [BookingsCreateComponent]
     }).compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(BookingsCreateComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,4 +25,15 @@ describe('BookingsCreateComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a form with controls', () => {
+    expect(component.form.controls.guestCount).toBeTruthy();
+    expect(component.form.controls.user.controls.firstName).toBeTruthy();
+    expect(component.form.controls.user.controls.lastName).toBeTruthy();
+    expect(component.form.controls.user.controls.address).toBeTruthy();
+    expect(component.form.controls.user.controls.country).toBeTruthy();
+  });
+
+  // An end-to-end test would make more sense here
+
 });
