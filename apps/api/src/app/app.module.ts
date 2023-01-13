bf86518818e3e94065a2f67a766782d945cff57d
Yassine Zeriouh <y@zeriouh.io>
2023-01-13 01:06:31 +0100

    refactor(api): remove boilerplate app controller and service

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,14 +1,10 @@
-import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
 import {BookingsModule} from "../bookings/bookings.module";
 
 @Module({
   imports: [
     BookingsModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
