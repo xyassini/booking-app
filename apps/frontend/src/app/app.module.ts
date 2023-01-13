793c0cbeaadb9c86ccb8f6154423d026d06b88aa
Yassine Zeriouh <y@zeriouh.io>
2023-01-13 02:42:43 +0100

    feat(frontend): add ngx-toastr

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -5,14 +5,18 @@ import {RouterModule} from '@angular/router';
 import {appRoutes} from './app.routes';
 import {HttpClientModule} from "@angular/common/http";
 import {ClassValidatorFormBuilderModule} from "ngx-reactive-form-class-validator";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ToastrModule} from "ngx-toastr";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     ClassValidatorFormBuilderModule.forRoot(),
     RouterModule.forRoot(appRoutes, {initialNavigation: 'enabledBlocking'}),
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent],
