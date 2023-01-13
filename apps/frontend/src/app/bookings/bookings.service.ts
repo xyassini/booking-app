d66fdd16779b17a5d98828f17563719bcb02c107
Yassine Zeriouh <y@zeriouh.io>
2023-01-13 02:24:14 +0100

    feat(bookings): implement getOne method in frontend service

--- apps/frontend/src/app/bookings/bookings.service.ts
+++ apps/frontend/src/app/bookings/bookings.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Booking} from "@prisma/client";
-import {Observable} from "rxjs";
+import {Booking, User} from "@prisma/client";
+import {BehaviorSubject, first, Observable, of, tap} from "rxjs";
 import {CreateBookingDto} from "@booking-app/dto";
 
 @Injectable({
@@ -9,14 +9,29 @@ import {CreateBookingDto} from "@booking-app/dto";
 })
 export class BookingsService {
 
+  currentBooking$: BehaviorSubject<Booking & { user: User } | undefined> = new BehaviorSubject<Booking & { user: User } | undefined>(undefined);
+
   constructor(private http: HttpClient) {
   }
 
-  getAll(): Observable<Booking[]> {
-    return this.http.get<Booking[]>('/api/bookings');
+  getAll(): Observable<Array<Booking & {user: User}>> {
+    return this.http.get<Array<Booking & {user: User}>>('/api/bookings');
   }
 
-  create(data: CreateBookingDto): Observable<Booking> {
-    return this.http.post<Booking>('/api/bookings', data);
+  getOne(id: string): Observable<Booking & { user: User }> {
+    if (this.currentBooking$.value?.id === id) {
+      // if the booking is already in the currentBooking$ subject, return it, then complete the stream
+      return of(this.currentBooking$.value).pipe(first());
+    } else {
+      return this.http.get<Booking & { user: User }>(`/api/bookings/${id}`).pipe(
+        tap(booking => this.currentBooking$.next(booking))
+      );
+    }
+  }
+
+  create(data: CreateBookingDto): Observable<Booking & { user: User }> {
+    return this.http.post<Booking & { user: User }>('/api/bookings', data).pipe(
+      tap(booking => this.currentBooking$.next(booking))
+    );
   }
 }
